0b5c56e3b52de34aa19adcdf11d3bd6ad7c111c5
Clément Bréan <Clem@clement.home>
2022-03-31 23:39:48 +0200

    k8

--- archive.js
+++ archive.js
@@ -84,3 +84,66 @@ var countBits = function(n) {
 console.log(countBits(1234));
 
 //K5 Descending Order
+// Your task is to make a function that can take any non-negative integer as an 
+//argument and return it with its digits in descending order. Essentially,
+// rearrange the digits to create the highest possible number.
+// Examples:
+// Input: 42145 Output: 54421
+// Input: 145263 Output: 654321
+// Input: 123456789 Output: 987654321
+//https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/javascript
+// function descendingOrder(n){
+//   return parseFloat(n.toString().split('').sort((a,b) => b-a).join(''));
+// }
+const descendingOrder = (n) => (parseFloat(n.toString().split('').sort((a,b) => b-a).join('')))
+console.log(descendingOrder(42145));
+
+//K6 isograms
+//https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/javascript
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+// Implement a function that determines whether a string that contains only letters is an isogram.
+//  Assume the empty string is an isogram. Ignore letter case.
+// Example: (Input --> Output
+// "Dermatoglyphics" --> true
+// "aba" --> false
+// "moOse" --> false (ignore letter case)
+function isIsogram(str){
+  let array = str.toLowerCase().split('')
+  const arrayLength = array.length;
+  let result = true
+  for (const item of array) {
+    array2 = array.filter(a => a !== item)
+    array2.length < arrayLength - 1 &&  (result = false)
+  }
+  return result;
+}
+console.log(isIsogram('Dermatoglyphics'));
+//smart solution with set that takes unique value
+function isIsogram(str){
+  return new Set(str.toUpperCase()).size == str.length;
+}
+
+//K7 two to one
+//https://www.codewars.com/kata/5656b6906de340bd1b0000ac/train/javascript
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string
+// , the longest possible, containing distinct letters - each taken only once - 
+// coming from s1 or s2.
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+// function longest(s1, s2) {
+//   //localeCompare compare to two string alphabetically with a return positive or negative
+//   //this is then use in the sort function
+//   const result = [...new Set(s1+s2)].sort((a,b) => a.localeCompare(b)).join('')
+//   return result;
+// }
+// const longest = (s1, s2) => ([...new Set(s1+s2)].sort((a,b) => a.localeCompare(b)).join(''))
+// console.log(longest('xyaabbbccccdefww', 'xxxxyyyyabklmopq'));
+//infact no need of localecompare...
+const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
+console.log(longest('xyaabbbccccdefww', 'xxxxyyyyabklmopq'));
+
+
